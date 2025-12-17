b786fcf8228365bfdb2bd8f3c784d59f203385d2
Alejo Hernandez <jomaherale13@gmail.com>
2025-12-17 09:41:47 -0600

    Expose JWT Tokenendpoint

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
 
     # 🔐 JWT
-    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/", TokenObtainPairView.as_view(), name="token"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 
     # 📦 API
